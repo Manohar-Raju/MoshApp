104e09dc2346441e099c70ebd2abe85d7c36efc7
Manohar_Raju1990 <manoharraj30@gmail.com>
2021-12-09 22:06:30 +0530

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyADzZAJuNkepI0lfODB46KhKK4vnqaWF-4",
+  authDomain: "moshapp-4e652.firebaseapp.com",
+  projectId: "moshapp-4e652",
+  storageBucket: "moshapp-4e652.appspot.com",
+  messagingSenderId: "512418971938",
+  appId: "1:512418971938:web:a32bba7e62b13c15f7a57b"
+  }
+};
